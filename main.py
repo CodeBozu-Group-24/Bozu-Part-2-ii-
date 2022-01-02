a62f557cb78cf8648e60a3641555c9fa6136877f
Prithwis Das <prithwisdas2017@gmail.com>
2022-01-02 08:20:51 +0530

    update

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ data = requests.get('https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump')
 soup = BeautifulSoup(data, 'lxml')
 titles = soup.findAll('a', class_='qa-heading-link lx-stream-post__header-link')
 #links = []
+writings = []
 for title in titles:
     link = "https://www.bbc.com"+title["href"]
     #data_new = requests.get(link).text
     #soup_new = BeautifulSoup(data_new, 'lxml')
-    print(getTextFromArticle(link))
+    writings.append(getTextFromArticle(link))
+    
