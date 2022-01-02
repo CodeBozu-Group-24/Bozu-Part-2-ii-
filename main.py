f4797d74072a4f2f17fa355738a84395057e0952
SentientPlatypus <trexxxxxxxxxxy@gmail.com>
2022-01-02 14:25:17 -0500

    fixed csvwriting issue

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ from csv import writer
 import pandas as pd
 from scraper import getTextFromArticle
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import re
 
 data = requests.get('https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump').text
 soup = BeautifulSoup(data, 'lxml')
 titles = soup.findAll('a', class_='qa-heading-link lx-stream-post__header-link')
+
+
+
 title_text = []
 for title in titles:
     title_text.append(title.get_text())
+
 #links = []
 writings = []
 for title in titles:
     link = "https://www.bbc.com"+title["href"]
     #data_new = requests.get(link).text
     #soup_new = BeautifulSoup(data_new, 'lxml')
-    writings.append(getTextFromArticle(link))
+    writingFinal = getTextFromArticle(link).encode("ascii", "ignore").decode()
+    writingFinal = re.sub("\n", " ", writingFinal)
+    writings.append(writingFinal)
 
 positivity = []    
 for writing in writings:
@@ -30,12 +37,12 @@ for writing in writings:
     positivity.append(positivity_score)
 
 
-with open('details.csv', 'a') as f:
+with open('details.csv', 'w') as f:
+    
     writer_object = writer(f)
-    details = []
-    for i in range(len(titles)):
-        details.append(title_text[i])
-        details.append(writings[i])
-        details.append(positivity[i])
-    writer_object.writerow(details)
-    f.close()    
+    writer_object.writerow(["title", "content", "score"])
+    for i in range(len(title_text)):
+        writer_object.writerow([title_text[i], writings[i], positivity[i]])
+    f.close()   
+ 
+print("done")
