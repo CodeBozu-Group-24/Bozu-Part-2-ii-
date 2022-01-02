e316053a395762c6febee3ac0166c6c744fe1528
SentientPlatypus <trexxxxxxxxxxy@gmail.com>
2022-01-01 21:43:01 -0500

    Added function to get text from link

---
+++ scraper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def getTextFromArticle(link:str):
+    req = requests.get(link).text
+    soup = BeautifulSoup(req, "lxml")
+    article = soup.find("article", {"class":"ssrcss-1mc1y2-ArticleWrapper e1nh2i2l6"})
+    textDivs = article.find_all("div", {"data-component":"text-block"})
+    returnString = ""
+    for textDiv in textDivs:
+        childDiv = textDiv.find_all("p")
+        for x in childDiv:
+            returnString+= x.get_text()
+        returnString += '\n'
+    
+    return returnString
+
+print(getTextFromArticle("https://www.bbc.com/news/world-us-canada-59703761"))
