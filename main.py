cd2bacf80641dd0f23bd16b67400bb75c245ecdc
Prithwis Das <prithwisdas2017@gmail.com>
2022-01-02 08:35:15 +0530

    update

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from csv import reader
 import pandas as pd
 from scraper import getTextFromArticle
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 data = requests.get('https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump').text
 soup = BeautifulSoup(data, 'lxml')
 titles = soup.findAll('a', class_='qa-heading-link lx-stream-post__header-link')
@@ -16,4 +18,20 @@ for title in titles:
     #data_new = requests.get(link).text
     #soup_new = BeautifulSoup(data_new, 'lxml')
     writings.append(getTextFromArticle(link))
-    
+
+positivity = []    
+for writing in writings:
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(writing)
+    positivity_score = sentiment_dict['pos']
+    positivity.append(positivity_score)
+
+with open('details.csv', 'a') as f:
+    writer_object = writer(f)
+    details = []
+    for i in range(len(titles)):
+        details.append(titles[i])
+        details.append(writings[i])
+        details.append(positivity[i])
+    writer_object.writerow(details)
+    f.close()    
