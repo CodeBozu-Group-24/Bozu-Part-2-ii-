dcbc82d9e8985de84e8da81519d2f560f72bd681
SentientPlatypus <trexxxxxxxxxxy@gmail.com>
2022-01-01 21:13:29 -0500

    got links

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ import pandas as pd
 data = requests.get('https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump').text
 soup = BeautifulSoup(data, 'lxml')
 titles = soup.findAll('a', class_='qa-heading-link lx-stream-post__header-link')
+links = []
 for title in titles:
     print(title.get_text())
+    print("https://www.bbc.com"+title["href"])
+    links.append("https://www.bbc.com"+title["href"])
+    print("-------------------")
+    
